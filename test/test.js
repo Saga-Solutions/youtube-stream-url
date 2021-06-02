cc0d6a62fdbd3fb1bd8105551252c8286bbd926c
Dulkith <dulkith@outlook.com>
2021-06-02 12:48:21 +0530

    api update

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 const Youtube = require('../src');
 
 (async () => {
-    console.log(await Youtube.getInfo({url: 'https://www.youtube.com/watch?v=LUU58iwi7NQ'}))
+    console.log(await Youtube.getInfo({ video_id: 'LUU58iwi7NQ' }))
 })();
